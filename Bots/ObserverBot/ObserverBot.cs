f050067b3248fc01089c728ea7e284f189b99f6a
gmcdonald73 <gmcdonald73@gmail.com>
2016-02-06 11:26:18 +1100

    observer bot now outputs player stats
    added player stats to observerbot

--- Bots/ObserverBot/ObserverBot.cs
+++ Bots/ObserverBot/ObserverBot.cs
@@ -6,10 +6,37 @@ namespace ObserverBot
 {
     public class ObserverBot : IHoldemPlayer
     {
+        private class PlayerStats
+        {
+            public int PlayerNum;
+            public int NumHandsPlayed;
+            public int NumPreFlopsFolded;
+            public int NumFlopsFolded;
+            public int NumTurnsFolded;
+            public int NumRiversFolded;
+            public int NumHandsWon;
+            public bool bWonsThisHand;
+            public bool bIsObserver;
+
+            public PlayerStats(int pPlayerNum, bool pbIsObserver)
+            {
+                PlayerNum = pPlayerNum;
+                bIsObserver = pbIsObserver;
+                NumHandsPlayed = 0;
+                NumPreFlopsFolded = 0;
+                NumFlopsFolded = 0;
+                NumTurnsFolded = 0;
+                NumRiversFolded = 0;
+                NumHandsWon = 0;
+                bWonsThisHand = false;
+            }
+        }
+
         private int _playerNum;
         private Card [] _board;
         private int _handNum = 0;
         TextWriter _tw;
+        private PlayerStats [] playerStats;
 
         public void InitPlayer(int playerNum)
         {
@@ -42,11 +69,39 @@ namespace ObserverBot
             // create a writer and open the file
             _handNum++;
 
+            if(_handNum == 1)
+            {
+                playerStats = new PlayerStats[numPlayers];
+
+                string sHeader = "HandNum\t";
+
+                foreach (PlayerInfo p in players)
+                {
+                    playerStats[p.PlayerNum] = new PlayerStats(p.PlayerNum, p.IsObserver);
+                    if(!p.IsObserver)
+                    {
+                        sHeader += p.PlayerNum + "-" + p.Name + "\t";
+                    }
+                }
+
+                // write a line of text to the file
+                _tw.WriteLine(sHeader);
+            }
+
             string sStackSizes = _handNum + "\t";
 
             foreach (PlayerInfo p in players)
             {
-                sStackSizes += p.StackSize + "\t";
+                if(!p.IsObserver)
+                {
+                    sStackSizes += p.StackSize + "\t";
+                }
+
+                if (p.IsAlive)
+                {
+                    playerStats[p.PlayerNum].NumHandsPlayed++;
+                    playerStats[p.PlayerNum].bWonsThisHand = false;
+                }
             }
 
             // write a line of text to the file
@@ -60,6 +115,34 @@ namespace ObserverBot
         public void SeeAction(EStage stage, int playerNum, EActionType action, int amount)
         {
             // this is called to inform you when any player (including yourself) makes an action (eg puts in blinds, checks, folds, calls, raises, or wins hand)
+            if (action == EActionType.ActionFold)
+            {
+                switch (stage)
+                {
+                    case EStage.StagePreflop:
+                        playerStats[playerNum].NumPreFlopsFolded++;
+                        break;
+                    case EStage.StageFlop:
+                        playerStats[playerNum].NumFlopsFolded++;
+                        break;
+                    case EStage.StageTurn:
+                        playerStats[playerNum].NumTurnsFolded++;
+                        break;
+                    case EStage.StageRiver:
+                        playerStats[playerNum].NumRiversFolded++;
+                        break;
+                }
+            }
+            else if (action == EActionType.ActionWin)
+            {
+                // be careful not to double count here. This can be call multiple times for the same player for the same hand if they win more than one side pot
+                if(!playerStats[playerNum].bWonsThisHand)
+                {
+                    playerStats[playerNum].NumHandsWon++;
+                    playerStats[playerNum].bWonsThisHand = true;
+                }
+            }
+
         }
 
         public void GetAction(EStage stage, int callAmount, int minRaise, int maxRaise, int raisesRemaining, int potSize, out EActionType yourAction, out int amount)
@@ -87,11 +170,34 @@ namespace ObserverBot
 
             foreach (PlayerInfo p in players)
             {
-                sStackSizes += p.StackSize + "\t";
+                if(!p.IsObserver)
+                {
+                    sStackSizes += p.StackSize + "\t";
+                }
             }
 
             // write a line of text to the file
             _tw.WriteLine(sStackSizes);
+
+            _tw.WriteLine("");
+
+            _tw.WriteLine("PlayerNum\tNumHandsPlayed\tNumPreFlopsFolded\tNumFlopsFolded\tNumTurnsFolded\tNumRiversFolded\tNumHandsWon");
+
+            foreach (PlayerStats p in playerStats)
+            {
+                if(!p.bIsObserver)
+                {
+                    _tw.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}",
+                            p.PlayerNum,
+                            p.NumHandsPlayed,
+                            p.NumPreFlopsFolded,
+                            p.NumFlopsFolded,
+                            p.NumTurnsFolded,
+                            p.NumRiversFolded,
+                            p.NumHandsWon
+                        );
+                }
+            }
 
             // close the stream
             _tw.Close();
